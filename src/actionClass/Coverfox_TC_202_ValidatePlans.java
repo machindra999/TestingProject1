6079bdc0ea5370d1a3e41b99d89865af1ccc05cd
machindra999 <machindrakale777@gmail.com>
2024-03-25 16:14:50 +0530

    [machindra]: new branch added

---
+++ src/actionClass/Coverfox_TC_202_ValidatePlans.java
@@ -0,0 +1,11 @@
+package actionClass;
+
+public class Coverfox_TC_202_ValidatePlans {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Added new branch validating..");
+
+	}
+
+}
